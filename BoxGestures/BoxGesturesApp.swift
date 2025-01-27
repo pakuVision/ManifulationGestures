2429588d7c6193ee85f0f47c42bdc15ed10dce59
pakuVision <boardguy.vision@gmail.com>
2025-01-27 21:55:03 +0900

    Initial Commit

---
+++ BoxGestures/BoxGesturesApp.swift
@@ -0,0 +1,33 @@
+//
+//  BoxGesturesApp.swift
+//  BoxGestures
+//
+//  Created by boardguy.vision on 2025/01/27.
+//
+
+import SwiftUI
+
+@main
+struct BoxGesturesApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ BoxGestures/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  BoxGestures
+//
+//  Created by boardguy.vision on 2025/01/27.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
